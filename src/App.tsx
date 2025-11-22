b47efb7932ff3a8bdc782e6da47457835d2b0f50
AminBadani <ahmadaminbadani@gmail.com>
2025-11-22 13:26:49 +0700

    membungkus route dengan  ModulContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import { HashRouter, Navigate, Route, Routes } from 'react-router'
+import { useState } from 'react'
+import { ModulContext } from './contexts/ModulContext'
 
 import Navbar from './components/Navbar'
 import Belajar from './pages/belajar'
 import Kuis from './pages/kuis'
 import Perkembangan from './pages/perkembangan'
 
+import Modul from './models/Modul'
+
 function App() {
+  const [listModul, setListModul] = useState<Modul[]>([]);
+
   return (
     <div className="min-h-screen bg-linear-to-br from-pink-100 via-purple-100 to-blue-100">
-      <HashRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Navigate to="/belajar" replace />} />
-          <Route path="/belajar" element={<Belajar />} />
-          <Route path="/kuis" element={<Kuis />} />
-          <Route path="/perkembangan" element={<Perkembangan />} />
-        </Routes>
-      </HashRouter>
+      <ModulContext.Provider value={{ listModul, setListModul }}>
+        <HashRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Navigate to="/belajar" replace />} />
+            <Route path="/belajar" element={<Belajar />} />
+            <Route path="/kuis" element={<Kuis />} />
+            <Route path="/perkembangan" element={<Perkembangan />} />
+          </Routes>
+        </HashRouter>
+      </ModulContext.Provider>
     </div>
   )
 }
