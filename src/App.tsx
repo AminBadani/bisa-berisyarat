ecf782ee47689d283d72a2f46ec77682b5b3aba2
AminBadani <ahmadaminbadani@gmail.com>
2025-11-22 23:33:57 +0700

    mengubah penamaan route menjadi bahasa inggris

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,23 @@
 import { HashRouter, Navigate, Route, Routes } from 'react-router'
-import { useState } from 'react'
-import { ModulContext } from './contexts/ModulContext'
 
 import Navbar from './components/Navbar'
-import Belajar from './pages/belajar'
-import Kuis from './pages/kuis'
-import Perkembangan from './pages/perkembangan'
-
-import Modul from './models/Modul'
+import Learn from './pages/learn'
+import Quiz from './pages/quiz'
+import Progress from './pages/progress'
 
 function App() {
-  const [listModul, setListModul] = useState<Modul[]>([]);
 
   return (
     <div className="min-h-screen bg-linear-to-br from-pink-100 via-purple-100 to-blue-100">
-      <ModulContext.Provider value={{ listModul, setListModul }}>
-        <HashRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Navigate to="/belajar" replace />} />
-            <Route path="/belajar" element={<Belajar />} />
-            <Route path="/kuis" element={<Kuis />} />
-            <Route path="/perkembangan" element={<Perkembangan />} />
-          </Routes>
-        </HashRouter>
-      </ModulContext.Provider>
+      <HashRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/learn" replace />} />
+          <Route path="/learn" element={<Learn />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/progress" element={<Progress />} />
+        </Routes>
+      </HashRouter>
     </div>
   )
 }
