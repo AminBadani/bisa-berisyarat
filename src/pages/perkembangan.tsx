1dad4d05294d63bedbeb62131b98441cb81501cf
AminBadani <ahmadaminbadani@gmail.com>
2025-11-22 09:34:18 +0700

    mempersiapkan halaman perkembangan

--- src/pages/perkembangan.tsx
+++ src/pages/perkembangan.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
+import { FaAward, FaChartLine, FaStar, FaTrophy } from "react-icons/fa";
+
 function Perkembangan() {
-    return (
-        <>
-            <h1>Perkembangan</h1>
-        </>
-    )
+  const [statistik, setStatistik] = useState('belajar');
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      
+    </div>
+  )
 }
 
 export default Perkembangan;
