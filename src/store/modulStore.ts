c21ca231289521a83eecd41bd2f7bdc1d67dd733
AminBadani <ahmadaminbadani@gmail.com>
2025-11-22 23:25:31 +0700

    install zustand dan buat moduleStore untuk menggantikan react context agar dapat membagikan (shared) modul antar halaman

---
+++ src/store/modulStore.ts
@@ -0,0 +1,31 @@
+import { create } from "zustand";
+import LearningModule from "../models/LearningModule";
+
+interface ModulState {
+    modules: LearningModule[];
+
+    setModules: (mods: LearningModule[]) => void;
+
+    // optional helpers
+    updateModule: (id: string, updated: LearningModule) => void;
+    addModule: (mod: LearningModule) => void;
+}
+
+export const useModulStore = create<ModulState>((set) => ({
+    modules: [],
+
+    setModules: (mods) => set({ modules: mods }),
+
+    updateModule: (id: string, updated: LearningModule) =>
+        set((state) => {
+            const newList = state.modules.map((m) =>
+                m.id === id ? updated : m
+            );
+            return { modules: newList };
+        }),
+
+    addModule: (mod) =>
+        set((state) => ({
+            modules: [...state.modules, mod],
+        })),
+}));
