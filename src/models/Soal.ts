7c4009f3145bfcf71022f2c27be9f2cd7475ba7f
AminBadani <ahmadaminbadani@gmail.com>
2025-11-22 09:30:47 +0700

    mengubah struktur class Soal

--- src/models/Soal.ts
+++ src/models/Soal.ts
@@ -1,39 +1,53 @@
 class Soal {
-    protected id: string;
+    protected id: number;
     protected tipe: string;
     protected soal: string;
-    protected gambar: string;
-    protected audio: string;
-    protected opsi: [];
-    protected jawabanBenar: [];
+    protected deskripsi: string;
+    protected opsi: any[];
+    protected jawabanBenar: number;
 
     constructor();
     constructor(
-        id: string,
+        id: number,
         tipe: string,
         soal: string,
-        gambar: string,
-        audio: string,
-        opsi: [],
-        jawabanBenar: []
+        deskripsi: string,
+        opsi: any[],
+        jawabanBenar: number
     )
     constructor(
-        id: string = '',
+        id: number = 0,
         tipe: string = '',
         soal: string = '',
-        gambar: string = '',
-        audio: string = '',
-        opsi: [] = [],
-        jawabanBenar: [] = []
+        deskripsi: string = '',
+        opsi: any[] = [],
+        jawabanBenar: number = -1,
     ) {
         this.id = id;
         this.tipe = tipe;
         this.soal = soal;
-        this.gambar = gambar;
-        this.audio = audio;
+        this.deskripsi = deskripsi;
         this.opsi = opsi;
         this.jawabanBenar = jawabanBenar;
     }
+
+    get getId() { return this.id }
+    set setId(value: number) { this.id = value }
+
+    get getTipe() { return this.tipe }
+    set setTipe(value: string) { this.tipe = value }
+    
+    get getSoal() { return this.soal }
+    set setSoal(value: string) { this.soal = value }
+    
+    get getDeskripsi() { return this.deskripsi }
+    set setDeskripsi(value: string) { this.deskripsi = value }
+
+    get getOpsi() { return this.opsi }
+    set setOpsi(value: []) { this.opsi = value }
+
+    get getJawabanBenar() { return this.jawabanBenar }
+    set setJawabanBenar(value: number) { this.jawabanBenar = value }
 }
 
 export default Soal;
